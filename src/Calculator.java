bc29c5051e32528cadbeac91a8e97d4532f9c514
playkoround <seasouphan@gmail.com>
2025-02-26 12:04:48 +0900

    refactor: improve addition method and allow user input

---
+++ src/Calculator.java
@@ -0,0 +1,12 @@
+package assignment_calculator;
+
+public class Calculator {
+
+    // 기능1: 더하기 메서드(기능)
+    int sum(int num1, int num2) {
+        System.out.println("num1 + num2 = " + (num1 + num2));
+        return num1 + num2;
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package assignment_calculator;
+
 public class Main {
     public static void main(String[] args) {
+        // calculator 객체화
+        Calculator calculator = new Calculator();
+
+        // 메서드 호출하기
+        calculator.sum(10, 20);
     }
 }
