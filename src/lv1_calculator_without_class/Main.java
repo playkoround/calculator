3e2a7ca0fdb7ef8f41fce5f8b2a5cf1c483c9109
playkoround <seasouphan@gmail.com>
2025-02-28 13:18:35 +0900

    Prevent division by zero and Improve user input instructions - Added a check to prvent division by zero - Remove unnecessary mention of double quotes when entering the operator

--- src/lv1_calculator_without_class/Main.java
+++ src/lv1_calculator_without_class/Main.java
@@ -5,7 +5,7 @@ import lv2_calculator_with_class.Calculator;
 import java.util.Scanner;
 
 public class Main {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         // First number
@@ -14,31 +14,36 @@ public class Main {
         scanner.nextLine(); // 엔터(\n) 제거
 
         // Operator
-        System.out.println( "Your 1st number is " + firstNumber + ".\nType your operator(+, -, /, *) with double quotes: ");
+        System.out.println("Your 1st number is " + firstNumber + ".\nType your operator(+, -, /, *) with double quotes: ");
         String operator = scanner.nextLine();
 
         // Second number
         System.out.println("Your operator is " + operator + ".\nType 2nd integer number: ");
         int secondNumber = scanner.nextInt();
-        System.out.println( "Your 2nd number is " + secondNumber + ".");
+        System.out.println("Your 2nd number is " + secondNumber + ".");
 
         // Calculation
         int result = 0;
-        switch (operator){
+        switch (operator) {
 
-                case "+":
+            case "+":
                 result = firstNumber + secondNumber;
                 break;
 
-                case "-":
+            case "-":
                 result = firstNumber - secondNumber;
                 break;
 
-                case "/":
+            case "/":
+                //반복문 쓰면 0 안넣을때까지 반복됨 > 가성비를 위해 위치생각해야함(효율성을 위하여)
+                if (secondNumber == 0) {
+                    System.out.println("Division by zero is not allowed.");
+                    return;
+                }
                 result = firstNumber / secondNumber;
                 break;
 
-                case "*":
+            case "*":
                 result = firstNumber * secondNumber;
                 break;
 
@@ -47,6 +52,7 @@ public class Main {
                 return;
         }
         System.out.println("You typed: " + firstNumber + " " + operator + " " + secondNumber + ". The result is " + result + ".");
-    }
 
+
+    }
 }
