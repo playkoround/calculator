8b6f7c53500219f06d7b1f17f0611d84b9914633
playkoround <seasouphan@gmail.com>
2025-03-05 15:18:11 +0900

    Many problems

--- src/lv2_calculator_with_class/Calculator.java
+++ src/lv2_calculator_with_class/Calculator.java
@@ -1,10 +1,10 @@
 package lv2_calculator_with_class;
+import java.util.Scanner;
 
 public class Calculator {
 
     // 1. 속성(변수)
-    private double num1;
-    private double num2;
+
 
 
     // 2. 생성자
@@ -12,17 +12,43 @@ public class Calculator {
 
 
 
-    // 3. 기능(메서드)
-//    add;
-//    subtraction;
-//    multiply;
-//    divide;
-//
-    // 3-1. 더하기 메서드(기능)
-    double sum(double num1, double num2) {
-        System.out.println("num1 + num2 = " + (num1 + num2));
-        return num1 + num2;
-    }
+    // 3. 기능(메서드) - 나중에는 클래스마다 롤을 줘서 나누어서 관리함 , 인터페이스를 만들어서 합치는 등
+//    Scanner
+    Scanner scanner = new Scanner(System.in);
 
 
+    // Calculation
+    public int calculator(int num1, int num2, String operator) {
+
+        int result = 0;
+        switch (operator) {
+
+            case "+":
+                result = num1 + num2;
+                break;
+
+            case "-":
+                result = num1 - num2;
+                break;
+
+            case "/":
+                //반복문 쓰면 0 안넣을때까지 반복됨 > 가성비를 위해 위치생각해야함(효율성을 위하여)
+                while (num2 == 0) {
+                    System.out.println("Division by zero is not allowed. Please type again.\nType 2nd integer number:");
+                    num2 = scanner.nextInt();
+                }
+                result = num1 / num2;
+                break;
+
+            case "*":
+                result = num1 * num2;
+                break;
+
+            default:
+                System.out.println("You typed something wrong.");
+                return 0;
+        }
+
+        return result;
+        }
 }
--- src/lv2_calculator_with_class/Main.java
+++ src/lv2_calculator_with_class/Main.java
@@ -1,11 +1,43 @@
 package lv2_calculator_with_class;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        // Scanner 생성
+        Scanner scanner = new Scanner(System.in);
+
         // calculator 객체화
         Calculator calculator = new Calculator();
 
-        // 메서드 호출하기
-        calculator.sum(10, 20);
-    }
+        // method 메서드 호출하기
+
+        boolean isFinished = false;
+        while(!isFinished) {
+            // First number
+            System.out.print("Type 1st integer number: ");
+            int num1 = scanner.nextInt();
+            scanner.nextLine(); // 엔터(\n) 제거
+
+            // Operator
+            System.out.println("Your 1st number is " + num1 + ".\nType your operator(+, -, /, *) with double quotes: ");
+            String operator = scanner.nextLine();
+
+            // Second number
+            System.out.println("Your operator is " + operator + ".\nType 2nd integer number: ");
+            int num2 = scanner.nextInt();
+            System.out.println("Your 2nd number is " + num2 + ".");
+
+            // Calculate
+
+
+
+            // Ask if it is finished
+            System.out.print("Type 'exit' to quit, type anything to keep running: ");
+            scanner.nextLine();
+            isFinished = scanner.nextLine().equals("exit");
+
+        }
+
+        }
 }
